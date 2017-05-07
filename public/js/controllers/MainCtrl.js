47bf14ffb73d7e08725ca73f9981f81e05944dc8
Prateek <psinha6@gmail.com>
2017-05-07 21:13:59 +0530

    First commit

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,10 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-	$scope.tagline = 'To the moon and back!';	
+	$scope.qrCode;
+	$scope.classNames = ['1', '2', '3', '4', '5', '6', '7', '8'];
+	//$scope.tagline = 'To the moon and back!';
+	$scope.generateQRCode = function(){
+		
+	}
 
 });
--- server.js
+++ server.js
@@ -26,5 +26,5 @@ require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
 app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
+console.log('Connect to port:: ' + port); 			// shoutout to the user
 exports = module.exports = app; 						// expose app
